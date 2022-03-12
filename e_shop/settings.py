9b0fd352448af2a03e912e95da78c06c473bbfeb
Fattijenishbek <fattijenishbek01@gmail.com>
2022-03-12 19:39:13 +0600

    settings changed

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.sites",
+    # "django.contrib.sites",
     "whitenoise.runserver_nostatic",
     "django_filters",
     "django_extensions",
